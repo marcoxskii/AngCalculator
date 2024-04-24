fe86a1870938c60f5df38cf22e3592c7c13230db
Addriank12 <addriank12@gmail.com>
2024-04-24 08:37:04 -0500

    Selector de operaciones

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 import { FooterComponent } from './components/footer/footer.component';
 import { BodyComponent } from './components/body/body.component';
 import { HeaderComponent } from './components/header/header.component';
+import { SumaComponent } from './pages/suma/suma.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, FooterComponent, BodyComponent, HeaderComponent],
+  imports: [RouterOutlet, FooterComponent, BodyComponent, HeaderComponent, SumaComponent],
 
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
