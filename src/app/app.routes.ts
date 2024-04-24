ab9cf6caf7f54b6ff3614c3dc091707f3810a5a3
Marco Cajamarca <marcoxc01@gmail.com>
2024-04-24 12:40:10 -0500

    adding home and about

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,13 +3,17 @@ import { SumaComponent } from './pages/suma/suma.component';
 import { RestaComponent } from './pages/resta/resta.component';
 import { DivisionComponent } from './pages/division/division.component';
 import{ MultiplicacionComponent } from './pages/multiplicacion/multiplicacion.component';
+import { HomeComponent } from './pages/home/home.component';
+import { AcercadeComponent } from './pages/acercade/acercade.component';
 
 
 
 export const routes: Routes = [
+    { path: 'home', component: HomeComponent },
     { path: 'suma', component: SumaComponent },
     { path: 'resta', component: RestaComponent },
     { path: 'division', component: DivisionComponent },
-    { path: 'multiplicacion', component: MultiplicacionComponent }
+    { path: 'multiplicacion', component: MultiplicacionComponent},
+    { path: 'acercaDe', component: AcercadeComponent}
     
 ];
